a02a617c703fc29a4159fdf8bef6a43139abccbb
valeriya.tarasova <val.tarasova@x5.ru>
2022-03-30 20:50:19 +0300

    test

--- src/test/java/com/example/demo/MainPageTest.java
+++ src/test/java/com/example/demo/MainPageTest.java
@@ -1,25 +1,14 @@
 package com.example.demo;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class MainPageTest {
     MainPage mainPage = new MainPage();
-
-    @BeforeAll
-    public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
 
     @BeforeEach
     public void setUp() {
@@ -30,25 +19,8 @@ public class MainPageTest {
     public void search() {
         mainPage.searchButton.click();
 
-        $("[data-test='search-input']").sendKeys("Selenium");
         $("button[data-test='full-search-button']").click();
 
         $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
-    }
-
-    @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.hover();
-
-        $("div[data-test='menu-main-popup-content']").shouldBe(visible);
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeAllToolsButton.click();
-
-        $("#products-page").shouldBe(visible);
-
-        assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
     }
 }
